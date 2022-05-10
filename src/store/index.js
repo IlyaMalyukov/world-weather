345c42cd40e8c34915be70f228ad816e1d082d80
Ilya Malyukov <ilya.maliukov@yandex.ru>
2022-05-10 20:31:30 +0300

    Сохранение карточек через vuex-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
 
 import cards from './modules/cards'
 
@@ -12,7 +13,12 @@ const store = new Vuex.Store({
   getters: {},
   modules: {
     cards
-  }
+  },
+  plugins: [
+    new VuexPersist({
+      modules: ['cards']
+    }).plugin
+  ]
 })
 
 export default store
